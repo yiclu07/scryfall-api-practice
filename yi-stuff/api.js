4a6825ca19b98b37a17aa1cd2bda4d50ad3ee3a2
yiclu07 <yi.c.lu07@gmail.com>
2024-10-28 00:29:15 -0400

    Add card search function

--- yi-stuff/api.js
+++ yi-stuff/api.js
@@ -1,1 +1,30 @@
-const url = "https://api.scryfall.com";
+const url = "https://api.scryfall.com/cards/search?q=name%3A";
+const header = { "User-Agent": "*/*", Accept: "application/json" };
+
+const cardSearchField = document.getElementById("card-search-text");
+const cardSearchFieldBtn = document.getElementById("card-search-text-btn");
+
+const fetchCards = async (e) => {
+  if (cardSearchField.value == "") {
+    alert("Please enter a search term!");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${url}${cardSearchField.value}`, header);
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleCardSearch = async (e) => {
+  const cardsData = await fetchCards();
+  console.log(cardsData);
+};
+
+cardSearchFieldBtn.addEventListener("click", handleCardSearch);
